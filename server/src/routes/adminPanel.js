52b7957df531e9d24b30da5b20d4381b0cfccfee
king <xulili456456@gmail.com>
2026-07-13 22:57:31 +0800

    Add no-cache headers to admin panel to prevent browser caching

--- server/src/routes/adminPanel.js
+++ server/src/routes/adminPanel.js
@@ -4,6 +4,9 @@ const path = require('path');
 const router = Router();
 
 router.get('/', (req, res) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
   res.sendFile(path.join(__dirname, '..', 'views', 'admin.html'));
 });
 
