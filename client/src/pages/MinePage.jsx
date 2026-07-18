d58a4e95d161bbbedc863a7dc1c41b9d986ca8d9
king <xulili456456@gmail.com>
2026-07-18 23:44:32 +0800

    Remove Inbox/Messages from Mine page

--- client/src/pages/MinePage.jsx
+++ client/src/pages/MinePage.jsx
@@ -95,7 +95,6 @@ export default function MinePage() {
       <div style={{margin:'0 16px 8px',background:'#fff',borderRadius:14,overflow:'hidden'}}>
         <MenuRow icon={<Users size={17} color="#4C6EF5"/>} iconBg="#EEF2FF" label={t('mine.myTeam')} desc={t('mine.myTeamDesc')} onClick={()=>navigate('/mine/team')}/>
         <MenuRow icon={<Store size={17} color="#FF5000"/>} iconBg="#FFF5F0" label={t('mine.myStore')} desc={t('mine.myStoreDesc')} onClick={()=>navigate('/store')}/>
-        <MenuRow icon={<Bell size={17} color="#8B5CF6"/>} iconBg="#F3E5F5" label={t('mine.messages')} desc={t('mine.messagesDesc')} onClick={()=>navigate('/mine/messages')}/>
       </div>
 
       {/* Account Group */}
