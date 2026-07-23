110c6acbcb6de15a88d3d42a68a5f74901c442ad
king <xulili456456@gmail.com>
2026-07-23 21:40:56 +0800

    Fix: conditional API URL for custom domain

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: window.location.hostname === 'www.shopeetrade.com' ? 'https://gift-platform-h6um.onrender.com/api' : '/api',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
